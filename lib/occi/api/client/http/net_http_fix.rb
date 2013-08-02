e6324c5301c1e7e1d1f5eb217327603738943faa
Boris Parak <parak@cesnet.cz>
2013-08-02 14:42:44 +0200

    Fixed cert chains in Ruby 2+

--- lib/occi/api/client/http/net_http_fix.rb
+++ lib/occi/api/client/http/net_http_fix.rb
@@ -5,17 +5,27 @@
 module Net
   class HTTP
 
-    if defined? SSL_ATTRIBUTES
-      # For Rubies 1.9.x
-      old_verbose, $VERBOSE = $VERBOSE, nil
-      SSL_ATTRIBUTES = SSL_ATTRIBUTES.concat %w(extra_chain_cert)
-      $VERBOSE = old_verbose
+    old_verbose, $VERBOSE = $VERBOSE, nil
+
+    if RUBY_VERSION =~ /^2\.0/
+      # For Rubies 2.0.x
+      SSL_IVNAMES = SSL_IVNAMES.concat [:@extra_chain_cert]
+      SSL_ATTRIBUTES = SSL_ATTRIBUTES.concat [:extra_chain_cert]
 
       attr_accessor :extra_chain_cert
-    else
+    elsif RUBY_VERSION =~ /^1\.9/
+      # For Rubies 1.9.x
+      SSL_ATTRIBUTES = SSL_ATTRIBUTES.concat %w(extra_chain_cert)
+
+      attr_accessor :extra_chain_cert
+    elsif RUBY_VERSION =~ /^1\.8/
       # For legacy Rubies 1.8.x
       ssl_context_accessor :extra_chain_cert
+    else
+      # Nothing, not sure what to do!
     end
+
+    $VERBOSE = old_verbose
 
   end
 end
