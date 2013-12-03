b7130d31408c1b32b07131fa457ae1b4beab0d40
Boris Parak <parak@cesnet.cz>
2013-12-03 19:12:55 +0100

    Updated Keystone authN plugin
    * Minor refactoring and clean-up
    * Added support for `snf-auth`
    * Fixes #15

--- lib/occi/api/client/http/authn_plugins/keystone.rb
+++ lib/occi/api/client/http/authn_plugins/keystone.rb
@@ -4,6 +4,8 @@ module Occi::Api::Client
 
       class Keystone < Base
 
+        KEYSTONE_URI_REGEXP = /^(Keystone|snf-auth) uri='(.+)'$/
+
         def setup(options = {})
           # get Keystone URL if possible, get unscoped token
           set_keystone_base_url
@@ -17,7 +19,8 @@ module Occi::Api::Client
         def authenticate(options = {})
           # OCCI-OS doesn't support HEAD method!
           response = @env_ref.class.get "#{@env_ref.endpoint}-/"
-          raise ::Occi::Api::Client::Errors::AuthnError, "Authentication failed with code #{response.code.to_s}!" unless response.success?
+          raise ::Occi::Api::Client::Errors::AuthnError,
+                "Authentication failed with code #{response.code.to_s}!" unless response.success?
         end
 
         private
@@ -27,16 +30,25 @@ module Occi::Api::Client
           Occi::Log.debug response.inspect
 
           return if response.success?
-          raise ::Occi::Api::Client::Errors::AuthnError, "Keystone AuthN failed with #{response.code.to_s}!" unless response.code == 401
+          raise ::Occi::Api::Client::Errors::AuthnError,
+                "Keystone AuthN failed with #{response.code.to_s}!" unless response.code == 401
 
-          unless response.headers['www-authenticate'] && response.headers['www-authenticate'].start_with?('Keystone')
-            raise ::Occi::Api::Client::Errors::AuthnError, "Target endpoint is probably not OpenStack, fallback failed!"
+          process_headers(response)
+        end
+
+        def process_headers(response)
+          authN_header = response.headers['www-authenticate']
+
+          if authN_header.blank?
+            raise ::Occi::Api::Client::Errors::AuthnError,
+                  "Response does not contain the www-authenticate header, fallback failed!"
           end
 
-          @keystone_url = /^Keystone uri='(.+)'$/.match(response.headers['www-authenticate'])[1]
-          raise ::Occi::Api::Client::Errors::AuthnError, "Unable to get Keystone's URL from the response!" unless @keystone_url
+          match = KEYSTONE_URI_REGEXP.match(authN_header)
+          raise ::Occi::Api::Client::Errors::AuthnError,
+                "Unable to get Keystone's URL from the response!" unless match && match[2]
 
-          @keystone_url = @keystone_url.chomp('/')
+          @keystone_url = match[2].chomp('/')
         end
 
         def set_auth_token(tenant = nil)
@@ -54,7 +66,8 @@ module Occi::Api::Client
           if response.success?
             @env_ref.class.headers['X-Auth-Token'] = response['access']['token']['id']
           else
-            raise ::Occi::Api::Client::Errors::AuthnError, "Unable to get a token from Keystone!"
+            raise ::Occi::Api::Client::Errors::AuthnError,
+                  "Unable to get a token from Keystone!"
           end
         end
 
@@ -71,7 +84,8 @@ module Occi::Api::Client
               }
             }
           else
-            raise ::Occi::Api::Client::Errors::AuthnError, "Unable to request a token from Keystone! Chosen AuthN not supported."
+            raise ::Occi::Api::Client::Errors::AuthnError,
+                  "Unable to request a token from Keystone! Chosen AuthN not supported."
           end
 
           body['auth']['tenantName'] = tenant if tenant && !tenant.empty?
@@ -90,9 +104,11 @@ module Occi::Api::Client
           Occi::Log.debug response.inspect
 
           # TODO: impl match with regexp in case of multiple tenants?
-          raise ::Occi::Api::Client::Errors::AuthnError, "Keystone didn't return any tenants!" unless response['tenants'] && response['tenants'].first
+          raise ::Occi::Api::Client::Errors::AuthnError,
+                "Keystone didn't return any tenants!" unless response['tenants'] && response['tenants'].first
           tenant = response['tenants'].first['name'] if response.success?
-          raise ::Occi::Api::Client::Errors::AuthnError, "Unable to get a tenant from Keystone!" unless tenant
+          raise ::Occi::Api::Client::Errors::AuthnError,
+                "Unable to get a tenant from Keystone!" unless tenant
 
           tenant
         end
