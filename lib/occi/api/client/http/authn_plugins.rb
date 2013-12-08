19cdc9d8b4ef9e1a9dd32e54f5fe11b1032f7311
Boris Parak <parak@cesnet.cz>
2013-12-09 00:43:17 +0100

    Making sure authn plugin base gets loaded first

--- lib/occi/api/client/http/authn_plugins.rb
+++ lib/occi/api/client/http/authn_plugins.rb
@@ -1,2 +1,3 @@
 # load all plugins
+require File.join(File.dirname(__FILE__), 'authn_plugins', 'base')
 Dir[File.join(File.dirname(__FILE__), 'authn_plugins', '*.rb')].each { |file| require file.gsub('.rb', '') }
