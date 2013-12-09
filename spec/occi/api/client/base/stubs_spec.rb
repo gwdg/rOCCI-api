98dda394dca3181f73c80fe1b9fff059b96af9ba
Zdenek Sustr <git@sustr.net>
2013-12-09 14:18:43 +0100

    A stubs stub

---
+++ spec/occi/api/client/base/stubs_spec.rb
@@ -0,0 +1,53 @@
+module Occi
+  module Api
+    module Client
+      module Base
+        module Stubs
+          describe 'Stubs' do
+       
+      context 'list' #do
+#        it 'raises NotImplementedError' do 
+#          expect{ Occi::Api::Client::Base::Stubs.list(nil) }.to raise_exception Occi::Api::Client::Errors::NotImplementedError
+#        end
+#      end
+
+      context 'describe' #do
+#       describe(resource_type_identifier=nil)
+#      end
+
+      context 'create' #do
+#       create(entity)
+#      end
+
+      context 'deploy' #do
+#       deploy(location)
+#      end
+
+      context 'deploy_ovf' #do
+#       deploy_ovf(descriptor)
+#      end
+
+      context 'deploy_ova' #do
+#      deploy_ova(descriptor)
+#      end
+
+      context 'delete' #do
+#       delete(resource_type_identifier)
+#      end
+
+      context 'trigger' #do
+#       trigger(resource_type_identifier, action_instance)
+#      end
+
+      context 'refresh' #do
+#       refresh
+#      end
+
+
+ 
+          end
+        end
+      end
+    end
+  end
+end
