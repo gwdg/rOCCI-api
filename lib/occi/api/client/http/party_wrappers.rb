d4349cdf4ab0c1bf8bc38d4b54444ed619850f6c
Boris Parak <parak@cesnet.cz>
2014-01-10 21:42:39 +0100

    Refactored HTTParty wrappers
    * Simplified codebase
    * Strict HTTP code checking
    * Fixed filtering on GET

--- lib/occi/api/client/http/party_wrappers.rb
+++ lib/occi/api/client/http/party_wrappers.rb
@@ -14,47 +14,24 @@ module Occi::Api::Client
       # @param filter [Occi::Collection] collection of filters
       # @return [Occi::Collection] parsed result of the request
       def get(path='/', filter=nil)
-        # apply filters if present
-        response = if filter
-          categories = filter.categories.collect { |category| category.to_text }.join(',')
-          attributes = filter.entities.collect { |entity|
-            entity.attributes.combine.collect { |k, v| k + '=' + v }
-          }.join(',')
+        raise ArgumentError, "Path is a required argument!" if path.blank?
 
-          headers = self.class.headers.clone
+        # apply filters if present
+        headers = self.class.headers.clone
+        unless filter.blank?
+          categories = filter.categories.to_a.collect { |category| category.to_string_short }.join(',')
+          attributes = filter.entities.to_a.collect { |entity| entity.attributes.to_header }.join(',')
+
           headers['Content-Type'] = 'text/occi'
           headers['Category'] = categories unless categories.empty?
-          headers['X-OCCI-Attributes'] = attributes unless attributes.empty?
-
-          self.class.get(path, :headers => headers)
-        else
-          self.class.get(path)
+          headers['X-OCCI-Attribute'] = attributes unless attributes.empty?
         end
 
+        response = self.class.get(path, :headers => headers)
         response_msg = response_message response
         raise "HTTP GET failed! #{response_msg}" unless response.code == 200
 
-        Occi::Log.debug "Response location: #{path.inspect}"
-        kind = @model.get_by_location(path) if @model
-
-        Occi::Log.debug "Response kind: #{kind.inspect}"
-
-        entity_type = nil
-        if kind && kind.related_to?(Occi::Core::Link)
-          entity_type = Occi::Core::Link
-        end
-
-        entity_type = Occi::Core::Resource unless entity_type
-
-        Occi::Log.debug "Parser call: #{response.content_type} #{path.include?('/-/')} " \
-                        "#{entity_type} #{response.headers.inspect}"
-        collection = Occi::Parser.parse(
-          response.content_type, response.body,
-          path.include?('/-/'), entity_type, response.headers
-        )
-
-        Occi::Log.debug "Parsed collection: empty? #{collection.empty?}"
-        collection
+        get_process_response(path, response)
       end
 
       # Performs POST requests and returns URI locations. Resource data must be provided
@@ -74,24 +51,9 @@ module Occi::Api::Client
       # @return [Occi::Collection, String, Boolean] Collection, URI location or action result (if ActionInstance is passed)
       def post(path, collection)
         raise ArgumentError, "Path is a required argument!" if path.blank?
+        raise ArgumentError, "Collection is a required argument!" if collection.blank?
 
-        headers = self.class.headers.clone
-        headers['Content-Type'] = @media_type
-
-        response = case @media_type
-        when 'application/occi+json'
-          self.class.post(path,
-                          :body => collection.to_json,
-                          :headers => headers)
-        when 'text/occi'
-          self.class.post(path,
-                          :headers => collection.to_header.merge(headers))
-        else
-          self.class.post(path,
-                          :body => collection.to_text,
-                          :headers => headers)
-        end
-
+        response = send_coll_request(path, collection)
         response_msg = response_message(response)
         raise "HTTP POST failed! #{response_msg}" unless response.code.between? 200, 201
 
@@ -108,28 +70,13 @@ module Occi::Api::Client
       # @return [Occi::Collection] parsed result of the request
       def put(path, collection)
         raise ArgumentError, "Path is a required argument!" if path.blank?
+        raise ArgumentError, "Collection is a required argument!" if collection.blank?
 
-        headers = self.class.headers.clone
-        headers['Content-Type'] = @media_type
-
-        response = case @media_type
-        when 'application/occi+json'
-          self.class.post(path,
-                          :body => collection.to_json,
-                          :headers => headers)
-        when 'text/occi'
-          self.class.post(path,
-                          :headers => collection.to_header.merge(headers))
-        else
-          self.class.post(path,
-                          :body => collection.to_text,
-                          :headers => headers)
-        end
-
+        response = send_coll_request(path, collection, :put)
         response_msg = response_message(response)
 
         if response.code.between? 200, 201
-          Occi::Parser.parse(response.header["content-type"].split(";").first, response.body)
+          Occi::Parser.parse(response.header["content-type"], response.body)
         else
           raise "HTTP POST failed! #{response_msg}"
         end
@@ -156,6 +103,54 @@ module Occi::Api::Client
 
       private
 
+      def get_process_response(path, response)
+        Occi::Log.debug "Response from location: #{path.inspect}"
+        kind = @model.get_by_location(path) if @model
+
+        Occi::Log.debug "Response should contain kind: #{kind.inspect}"
+        entity_type = nil
+        if kind && kind.related_to?(Occi::Core::Link)
+          entity_type = Occi::Core::Link
+        end
+        entity_type = Occi::Core::Resource unless entity_type
+
+        Occi::Log.debug "Parser call: #{response.content_type} #{path.include?('/-/')} " \
+                        "#{entity_type} #{response.headers.inspect}"
+        collection = Occi::Parser.parse(
+          response.content_type, response.body,
+          path.include?('/-/'), entity_type, response.headers
+        )
+
+        Occi::Log.debug "Parsed collection: empty? #{collection.empty?}"
+        collection
+      end
+
+      def send_coll_request(path, collection, type = :post)
+        type ||= :post
+        raise ArgumentError, "Unsupported send " \
+                             "type #{type.to_s.inspect}!" unless [:post, :put].include?(type)
+
+        headers = self.class.headers.clone
+        headers['Content-Type'] = @media_type
+
+        case @media_type
+        when 'application/occi+json'
+          self.class.send type,
+                          path,
+                          :body => collection.to_json,
+                          :headers => headers
+        when 'text/occi'
+          self.class.send type,
+                          path,
+                          :headers => collection.to_header.merge(headers)
+        else
+          self.class.send type,
+                          path,
+                          :body => collection.to_text,
+                          :headers => headers
+        end
+      end
+
       def post_action(response)
         true
       end
@@ -163,13 +158,13 @@ module Occi::Api::Client
       def post_create(response)
         if response.code == 200
           collection = Occi::Parser.parse(
-            response.header["content-type"].split(";").first,
+            response.header["content-type"],
             response.body
           )
 
           if collection.empty?
             Occi::Parser.locations(
-              response.header["content-type"].split(";").first,
+              response.header["content-type"],
               response.body,
               response.headers
             ).first
@@ -180,14 +175,12 @@ module Occi::Api::Client
           end
         else
           Occi::Parser.locations(
-            response.header["content-type"].split(";").first,
+            response.header["content-type"],
             response.body,
             response.headers
           ).first
         end
       end
-
-      def post_update(response); end
 
     end
 
