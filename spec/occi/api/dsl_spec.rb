5f4abb381a96c8db87eac7dbac22b319d4ba8741
Zdenek Sustr <git@sustr.net>
2014-02-11 16:33:37 +0100

    Essential .connect specs

--- spec/occi/api/dsl_spec.rb
+++ spec/occi/api/dsl_spec.rb
@@ -2,14 +2,62 @@ require 'rspec'
 
 module Occi
   module Api
-      describe Dsl do
 
-        context "using ClientHttp" do
+    vcr_options = { :record => :new_episodes }
 
-          it "does something"
+    describe Dsl, :vcr => vcr_options do
+      class DummyClass
+      end
 
+      before(:each) do
+        @dummy = DummyClass.new
+        @dummy.extend Occi::Api::Dsl
+      end
+
+      context ".connect" do
+
+        it "connects successfully through HTTP" do
+
+          expect(
+            @dummy.connect(:http,
+            {
+              :endpoint => ENV['ROCCI_SPEC_ENDPOINT'] || 'https://localhost:3300',
+              :auth => hash_or_nil_helper( ENV['ROCCI_SPEC_AUTH_JSON'] ) || { :type  => "none" },
+              :log => { :out   => "/dev/null",
+                        :level => Occi::Log::DEBUG },
+              :auto_connect => true,
+              :media_type => "text/plain,text/occi"
+            } )).to eql true
         end
 
+        it "raises exception for unsupported protocol" do
+
+          expect{
+            @dummy.connect(:ftp,
+            { 
+              :endpoint => ENV['ROCCI_SPEC_ENDPOINT'] || 'https://localhost:3300',
+              :auth => hash_or_nil_helper( ENV['ROCCI_SPEC_AUTH_JSON'] ) || { :type  => "none" },
+              :log => { :out   => "/dev/null",
+                        :level => Occi::Log::DEBUG },
+              :auto_connect => true,
+              :media_type => "text/plain,text/occi"
+            } ) }.to raise_exception(ArgumentError)
+        end
+
+        it "accepts block options" do
+          expect(
+            @dummy.connect(:http,          
+              :endpoint => ENV['ROCCI_SPEC_ENDPOINT'] || 'https://localhost:3300',
+              :auth => hash_or_nil_helper( ENV['ROCCI_SPEC_AUTH_JSON'] ) || { :type  => "none" },
+              :log => { :out   => "/dev/null",
+                        :level => Occi::Log::DEBUG },
+              :auto_connect => true,
+              :media_type => "text/plain,text/occi")).to eql true
+        end
+
+
       end
+
+    end
   end
 end
