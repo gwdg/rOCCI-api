4673bd1ca78022a5406832dabb835a811b7c171d
Enol Fernandez <enol.fernandez@egi.eu>
2017-06-14 12:10:34 +0100

    Add CA options to token authn
    When using token based authentication allow passing CA related options
    to verify server certificate.

--- lib/occi/api/client/http/authn_plugins/token.rb
+++ lib/occi/api/client/http/authn_plugins/token.rb
@@ -9,6 +9,9 @@ module Occi::Api::Client
           raise ArgumentError, "Missing required option 'token' for token auth!" if @options[:token].blank?
           raise ArgumentError, "Token cannot be a multi-line string!" if @options[:token].strip.lines.count > 1
           @env_ref.class.headers['X-Auth-Token'] = @options[:token].strip
+
+          @env_ref.class.ssl_ca_path @options[:ca_path] if @options[:ca_path]
+          @env_ref.class.ssl_ca_file @options[:ca_file] if @options[:ca_file]
         end
 
       end
